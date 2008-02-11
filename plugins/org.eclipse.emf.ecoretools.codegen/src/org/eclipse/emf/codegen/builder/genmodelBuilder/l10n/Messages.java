af4c3e0459c7b1ff4081032d0361b277adbffb44
lbigearde <lbigearde>
2008-02-11 23:13:27 +0000

    added D Webber contribution : http://dev.eclipse.org/ipzilla/show_bug.cgi?id=2058

---
+++ plugins/org.eclipse.emf.ecoretools.codegen/src/org/eclipse/emf/codegen/builder/genmodelBuilder/l10n/Messages.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Lucas Bigeardel. All rights reserved. This program
+ * and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Messages.java
+ * 
+ * Contributors: 
+ * 		Lucas Bigeardel - initial API and implementation
+ * 
+ ******************************************************************************/
+
+package org.eclipse.emf.codegen.builder.genmodelBuilder.l10n;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ * l10n localization 
+ * 
+ * @author <a href="mailto:lucas.bigeardel@gmail.com">lucas.bigeardel@gmail.com</a>
+ *
+ */
+public class Messages {
+	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(BUNDLE_NAME);
+
+	private Messages() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
