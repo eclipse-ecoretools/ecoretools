e2649dd198677732ed6fcd725d9941b9927bdcc8
lbigearde <lbigearde>
2008-05-12 21:48:46 +0000

    clean EPL header for NsURIComparator file

--- plugins/org.eclipse.emf.ecoretools.registration/src/org/eclipse/emf/ecoretools/registration/internal/NsURIComparator.java
+++ plugins/org.eclipse.emf.ecoretools.registration/src/org/eclipse/emf/ecoretools/registration/internal/NsURIComparator.java
@@ -9,18 +9,7 @@
  * Contributors:
  *    INRIA - initial API and implementation
  *
- * $Id: NsURIComparator.java,v 1.2 2008/04/28 15:47:42 jlescot Exp $
- **********************************************************************/
-/* **********************************************************************
- * Copyright (c) 2007, 2008 INRIA and others
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    INRIA - initial API and implementation
+ * $Id: NsURIComparator.java,v 1.3 2008/05/12 21:48:46 lbigearde Exp $
  **********************************************************************/
 
 package org.eclipse.emf.ecoretools.registration.internal;
