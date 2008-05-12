071a62bdc8d9edd2d9e8aeab24698d682a89befd
lbigearde <lbigearde>
2008-05-12 21:49:11 +0000

    clean EPL header for PackageNameComparator file

--- plugins/org.eclipse.emf.ecoretools.registration/src/org/eclipse/emf/ecoretools/registration/internal/PackageNameComparator.java
+++ plugins/org.eclipse.emf.ecoretools.registration/src/org/eclipse/emf/ecoretools/registration/internal/PackageNameComparator.java
@@ -9,18 +9,7 @@
  * Contributors:
  *    INRIA - initial API and implementation
  *
- * $Id: PackageNameComparator.java,v 1.2 2008/04/28 15:47:42 jlescot Exp $
- **********************************************************************/
-/* **********************************************************************
- * Copyright (c) 2007, 2008 INRIA and others
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    INRIA - initial API and implementation
+ * $Id: PackageNameComparator.java,v 1.3 2008/05/12 21:49:11 lbigearde Exp $
  **********************************************************************/
 
 package org.eclipse.emf.ecoretools.registration.internal;
@@ -52,12 +41,12 @@ public class PackageNameComparator extends RegisteredPackageComparator {
 		EPackage p2 = (EPackage) e2;
 
 		switch (getOrder()) {
-		case ASCENDING_ORDER:
-			return super.compare(viewer, p1.getName(), p2.getName());
-		case DESCENDING_ORDER:
-			return super.compare(viewer, p2.getName(), p1.getName());
-		default:
-			return 0;
+			case ASCENDING_ORDER:
+				return super.compare(viewer, p1.getName(), p2.getName());
+			case DESCENDING_ORDER:
+				return super.compare(viewer, p2.getName(), p1.getName());
+			default:
+				return 0;
 		}
 	}
 
