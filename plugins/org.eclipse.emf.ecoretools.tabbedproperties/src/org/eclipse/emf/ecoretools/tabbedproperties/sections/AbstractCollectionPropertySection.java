37716b4b85343dadfcbc8edbf2512a56fbb53f0b
jlescot <jlescot>
2009-06-02 12:46:54 +0000

    Just fix header typo

--- plugins/org.eclipse.emf.ecoretools.tabbedproperties/src/org/eclipse/emf/ecoretools/tabbedproperties/sections/AbstractCollectionPropertySection.java
+++ plugins/org.eclipse.emf.ecoretools.tabbedproperties/src/org/eclipse/emf/ecoretools/tabbedproperties/sections/AbstractCollectionPropertySection.java
@@ -1,12 +1,16 @@
 /***************************************************************************
  * Copyright (c) 2008 Conselleria de Infraestructuras y Transporte,
- * Generalitat de la Comunitat Valenciana . All rights reserved. This program
- * and the accompanying materials are made available under the terms of the
- * Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+ * Generalitat de la Comunitat Valenciana and ohers.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors: Mario Cervera Ubeda (Integranova) – initial API and implementation
+ * Contributors:
+ *    Mario Cervera Ubeda (Integranova) - initial API and implementation
  *
+ * $Id: AbstractCollectionPropertySection.java,v 1.3 2009/06/02 12:46:54 jlescot Exp $
  ******************************************************************************/
 package org.eclipse.emf.ecoretools.tabbedproperties.sections;
 
