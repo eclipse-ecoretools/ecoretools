9ebc6766527ce2da5e765fcaf2f318449b3527eb
cbrun <cedric.brun@obeo.fr>
2014-04-15 18:13:51 +0200

    [432804] Introduce an .ecore and .xcore files actions to create diagrams
    The action will figure out if there is already a session opened on this
    model or not. If not it will ask for the .aird file to create and then
    prompt to create a new representation.

---
+++ org.eclipse.emf.ecoretools.design/src/org/eclipse/emf/ecoretools/design/editor/EcoreToolsViewpoints.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.ecoretools.design.editor;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.sirius.business.api.componentization.ViewpointRegistry;
+import org.eclipse.sirius.viewpoint.description.Viewpoint;
+
+public class EcoreToolsViewpoints {
+
+	private ViewpointRegistry registry;
+
+	public EcoreToolsViewpoints(ViewpointRegistry registry) {
+		this.registry = registry;
+	}
+
+	public Viewpoint design() {
+		return registry
+				.getViewpoint(URI
+						.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Design"));
+	}
+
+	public Viewpoint archetype() {
+		return registry
+				.getViewpoint(URI
+						.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Archetype"));
+	}
+
+	public Viewpoint review() {
+		return registry
+				.getViewpoint(URI
+						.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Review"));
+	}
+
+	public Viewpoint generation() {
+		return registry
+				.getViewpoint(URI
+						.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Generation"));
+	}
+
+	public static EcoreToolsViewpoints fromViewpointRegistry() {
+		return new EcoreToolsViewpoints(ViewpointRegistry.getInstance());
+	}
+}
