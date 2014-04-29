d24fb7bd7eb40c11f309ff8c7b119a75b1645a09
Laurent Redor <laurent.redor@obeo.fr>
2014-04-29 14:23:46 +0200

    [433756] Use reflection for EcorePlugin.computePlatformURIMap call
    The API has changed for method EcorePlugin.computePlatformURIMap(). The
    reflection is a way to address this problem.
    
    Bug: 433756
    Change-Id: I79b43e19bac0b756950f31b2e08e7b3d4f5d63b7
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

--- org.eclipse.emf.ecoretools.design/src/org/eclipse/emf/ecoretools/design/service/EcoreToolsDesignPlugin.java
+++ org.eclipse.emf.ecoretools.design/src/org/eclipse/emf/ecoretools/design/service/EcoreToolsDesignPlugin.java
@@ -10,10 +10,16 @@
  *******************************************************************************/
 package org.eclipse.emf.ecoretools.design.service;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.sirius.business.api.componentization.ViewpointRegistry;
@@ -30,86 +36,106 @@ import org.osgi.framework.BundleContext;
  *         href="mailto:laurent.goubet@obeo.fr">laurent.goubet@obeo.fr</a>
  */
 public class EcoreToolsDesignPlugin extends Plugin {
-	/** The plug-in ID. */
-	public static final String PLUGIN_ID = "org.eclipse.emf.ecoretools.design"; //$NON-NLS-1$
+    /** The plug-in ID. */
+    public static final String PLUGIN_ID = "org.eclipse.emf.ecoretools.design"; //$NON-NLS-1$
 
-	/** This plug-in's shared instance. */
-	private static EcoreToolsDesignPlugin plugin;
+    /** This plug-in's shared instance. */
+    private static EcoreToolsDesignPlugin plugin;
 
-	private static Set<Viewpoint> viewpoints;
+    private static Set<Viewpoint> viewpoints;
 
-	private SessionManagerListener notifWhenSessionAreCreated;
+    private SessionManagerListener notifWhenSessionAreCreated;
 
-	/**
-	 * Default constructor for the plugin.
-	 */
-	public EcoreToolsDesignPlugin() {
-		plugin = this;
-	}
+    /**
+     * Default constructor for the plugin.
+     */
+    public EcoreToolsDesignPlugin() {
+        plugin = this;
+    }
 
-	/**
-	 * Returns the shared instance.
-	 * 
-	 * @return the shared instance
-	 */
-	public static EcoreToolsDesignPlugin getDefault() {
-		return plugin;
-	}
+    /**
+     * Returns the shared instance.
+     * 
+     * @return the shared instance
+     */
+    public static EcoreToolsDesignPlugin getDefault() {
+        return plugin;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void start(final BundleContext context) throws Exception {
-		super.start(context);
-		viewpoints = new HashSet<Viewpoint>();
-		viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(
-				PLUGIN_ID + "/description/ecore.odesign"));
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(final BundleContext context) throws Exception {
+        super.start(context);
+        viewpoints = new HashSet<Viewpoint>();
+        viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/ecore.odesign"));
 
-		notifWhenSessionAreCreated = new SessionManagerListener.Stub() {
+        notifWhenSessionAreCreated = new SessionManagerListener.Stub() {
 
-			@Override
-			public void notifyAddSession(Session newSession) {
-				ResourceSet set = newSession.getTransactionalEditingDomain()
-						.getResourceSet();
-				set.getURIConverter().getURIMap()
-						.putAll(EcorePlugin.computePlatformURIMap(true));
-				newSession.getEventBroker().addLocalTrigger(
-						GenModelAutoReload.SHOULD_RELOAD,
-						new GenModelAutoReload(newSession
-								.getTransactionalEditingDomain()));
+            @SuppressWarnings("unchecked")
+            @Override
+            public void notifyAddSession(Session newSession) {
+                ResourceSet set = newSession.getTransactionalEditingDomain().getResourceSet();
+                Map<URI, URI> result = null;
+                // Invoke computePlatformURIMap by reflection because this API
+                // change in EMF
+                try {
+                    Method computePlatformURIMap = EcorePlugin.class.getMethod("computePlatformURIMap", Boolean.class);
+                    result = (Map<URI, URI>) computePlatformURIMap.invoke(null, true);
+                } catch (NoSuchMethodException e) {
+                } catch (IllegalAccessException e) {
+                } catch (IllegalArgumentException e) {
+                } catch (InvocationTargetException e) {
+                }
+                try {
+                    Method computePlatformURIMap = EcorePlugin.class.getMethod("computePlatformURIMap");
+                    result = (Map<URI, URI>) computePlatformURIMap.invoke(null);
+                } catch (NoSuchMethodException e) {
+                } catch (IllegalAccessException e) {
+                } catch (IllegalArgumentException e) {
+                } catch (InvocationTargetException e) {
+                }
 
-				newSession.getEventBroker().addLocalTrigger(
-						GenModelUpdateGenFeatureContainment.SHOULD_UPDATE,
-						new GenModelUpdateGenFeatureContainment(newSession));
-			}
-		};
-		SessionManager.INSTANCE.addSessionsListener(notifWhenSessionAreCreated);
+                if (result != null) {
+                    set.getURIConverter().getURIMap().putAll(result);
+                } else {
+                    IStatus status = new Status(IStatus.WARNING, EcoreToolsDesignPlugin.PLUGIN_ID,
+                            "The EMF API EcorePlugin.computePlatformURIMap has probably changed and is not supported yet by EcoreTools.");
+                    EcoreToolsDesignPlugin.getDefault().getLog().log(status);
 
-	}
+                }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void stop(final BundleContext context) throws Exception {
-		plugin = null;
-		if (viewpoints != null) {
-			for (final Viewpoint viewpoint : viewpoints) {
-				ViewpointRegistry.getInstance().disposeFromPlugin(viewpoint);
-			}
-			viewpoints.clear();
-			viewpoints = null;
-		}
-		if (notifWhenSessionAreCreated != null) {
-			SessionManager.INSTANCE
-					.removeSessionsListener(notifWhenSessionAreCreated);
-		}
+                newSession.getEventBroker().addLocalTrigger(GenModelAutoReload.SHOULD_RELOAD, new GenModelAutoReload(newSession.getTransactionalEditingDomain()));
 
-		super.stop(context);
-	}
+                newSession.getEventBroker().addLocalTrigger(GenModelUpdateGenFeatureContainment.SHOULD_UPDATE, new GenModelUpdateGenFeatureContainment(newSession));
+            }
+        };
+        SessionManager.INSTANCE.addSessionsListener(notifWhenSessionAreCreated);
+
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(final BundleContext context) throws Exception {
+        plugin = null;
+        if (viewpoints != null) {
+            for (final Viewpoint viewpoint : viewpoints) {
+                ViewpointRegistry.getInstance().disposeFromPlugin(viewpoint);
+            }
+            viewpoints.clear();
+            viewpoints = null;
+        }
+        if (notifWhenSessionAreCreated != null) {
+            SessionManager.INSTANCE.removeSessionsListener(notifWhenSessionAreCreated);
+        }
+
+        super.stop(context);
+    }
 }
