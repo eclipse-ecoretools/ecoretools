5bc8014e664f3f8db41d87ffe662d8d7be12570d
cbrun <cedric.brun@obeo.fr>
2015-03-23 13:22:29 +0100

    [462465] Only deselect VPs from EcoreTools and keep the others enabled

--- org.eclipse.emf.ecoretools.design.ui/src/org/eclipse/emf/ecoretools/design/ui/action/EcoreInitDiagramFileAction.java
+++ org.eclipse.emf.ecoretools.design.ui/src/org/eclipse/emf/ecoretools/design/ui/action/EcoreInitDiagramFileAction.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.emf.ecoretools.design.ui.action;
 
+import java.util.Set;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -42,206 +44,155 @@ import org.eclipse.ui.PlatformUI;
 
 public class EcoreInitDiagramFileAction implements IObjectActionDelegate {
 
-	private IWorkbenchPart targetPart;
+    private IWorkbenchPart targetPart;
 
-	private URI domainModelURI;
+    private URI domainModelURI;
 
-	private IProject containingProject;
+    private IProject containingProject;
 
-	private IStructuredSelection selection;
+    private IStructuredSelection selection;
 
-	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		this.targetPart = targetPart;
-	}
+    public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+        this.targetPart = targetPart;
+    }
 
-	public void selectionChanged(IAction action, ISelection selection) {
-		domainModelURI = null;
-		action.setEnabled(false);
-		if (selection instanceof IStructuredSelection == false
-				|| selection.isEmpty()) {
-			return;
-		}
-		this.selection = (IStructuredSelection) selection;
-		if (this.selection.getFirstElement() instanceof IFile) {
-			IFile file = (IFile) this.selection.getFirstElement();
-			containingProject = file.getProject();
-			domainModelURI = URI.createPlatformResourceURI(file.getFullPath()
-					.toString(), true);
-			action.setEnabled(true);
-		}
-	}
+    public void selectionChanged(IAction action, ISelection selection) {
+        domainModelURI = null;
+        action.setEnabled(false);
+        if (selection instanceof IStructuredSelection == false || selection.isEmpty()) {
+            return;
+        }
+        this.selection = (IStructuredSelection) selection;
+        if (this.selection.getFirstElement() instanceof IFile) {
+            IFile file = (IFile) this.selection.getFirstElement();
+            containingProject = file.getProject();
+            domainModelURI = URI.createPlatformResourceURI(file.getFullPath().toString(), true);
+            action.setEnabled(true);
+        }
+    }
 
-	private Shell getShell() {
-		return targetPart.getSite().getShell();
-	}
+    private Shell getShell() {
+        return targetPart.getSite().getShell();
+    }
 
-	public void run(IAction action) {
-		Session existingSession = null;
+    public void run(IAction action) {
+        Session existingSession = null;
 
-		for (Session session : SessionManager.INSTANCE.getSessions()) {
-			ResourceSet set = session.getTransactionalEditingDomain()
-					.getResourceSet();
-			for (Resource res : set.getResources()) {
-				if (res.getURI() != null) {
-					if (set.getURIConverter()
-							.normalize(res.getURI())
-							.equals(set.getURIConverter().normalize(
-									domainModelURI))) {
-						existingSession = session;
-					}
-				}
-			}
-		}
-		if (existingSession == null) {
-			/*
-			 * we could not find a session already having this file in its
-			 * resources. We will use the containing project if it is a modeling
-			 * project but we have to add the resource as a semantic resource.
-			 */
-			final Option<ModelingProject> prj = ModelingProject
-					.asModelingProject(containingProject);
-			if (prj.some()) {
-				existingSession = prj.get().getSession();
-				if (existingSession == null) {
-					Option<URI> optionalMainSessionFileURI = prj.get()
-							.getMainRepresentationsFileURI(
-									new NullProgressMonitor(), false, false);
-					if (optionalMainSessionFileURI.some()) {
-						// Load the main representations file of this modeling
-						// project if it's not already loaded or during loading.
-						ModelingProjectManager.INSTANCE
-								.loadAndOpenRepresentationsFile(optionalMainSessionFileURI
-										.get());
-					}
-				}
-				if (OpenRepresentationsFileJob.shouldWaitOtherJobs()) {
-					// We are loading session(s), wait loading is finished
-					// before continuing.
-					OpenRepresentationsFileJob.waitOtherJobs();
-				}
-				existingSession = prj.get().getSession();
-				if (existingSession != null) {
-					existingSession
-							.getTransactionalEditingDomain()
-							.getCommandStack()
-							.execute(
-									new RecordingCommand(existingSession
-											.getTransactionalEditingDomain()) {
+        for (Session session : SessionManager.INSTANCE.getSessions()) {
+            ResourceSet set = session.getTransactionalEditingDomain().getResourceSet();
+            for (Resource res : set.getResources()) {
+                if (res.getURI() != null) {
+                    if (set.getURIConverter().normalize(res.getURI()).equals(set.getURIConverter().normalize(domainModelURI))) {
+                        existingSession = session;
+                    }
+                }
+            }
+        }
+        if (existingSession == null) {
+            /*
+             * we could not find a session already having this file in its
+             * resources. We will use the containing project if it is a modeling
+             * project but we have to add the resource as a semantic resource.
+             */
+            final Option<ModelingProject> prj = ModelingProject.asModelingProject(containingProject);
+            if (prj.some()) {
+                existingSession = prj.get().getSession();
+                if (existingSession == null) {
+                    Option<URI> optionalMainSessionFileURI = prj.get().getMainRepresentationsFileURI(new NullProgressMonitor(), false, false);
+                    if (optionalMainSessionFileURI.some()) {
+                        // Load the main representations file of this modeling
+                        // project if it's not already loaded or during loading.
+                        ModelingProjectManager.INSTANCE.loadAndOpenRepresentationsFile(optionalMainSessionFileURI.get());
+                    }
+                }
+                if (OpenRepresentationsFileJob.shouldWaitOtherJobs()) {
+                    // We are loading session(s), wait loading is finished
+                    // before continuing.
+                    OpenRepresentationsFileJob.waitOtherJobs();
+                }
+                existingSession = prj.get().getSession();
+                if (existingSession != null) {
+                    existingSession.getTransactionalEditingDomain().getCommandStack().execute(new RecordingCommand(existingSession.getTransactionalEditingDomain()) {
 
-										@Override
-										protected void doExecute() {
-											prj.get()
-													.getSession()
-													.addSemanticResource(
-															domainModelURI,
-															new NullProgressMonitor());
+                        @Override
+                        protected void doExecute() {
+                            prj.get().getSession().addSemanticResource(domainModelURI, new NullProgressMonitor());
 
-										}
-									});
-					existingSession.addSemanticResource(domainModelURI,
-							new NullProgressMonitor());
-				}
+                        }
+                    });
+                    existingSession.addSemanticResource(domainModelURI, new NullProgressMonitor());
+                }
 
-			}
-		}
+            }
+        }
 
-		if (existingSession == null) {
-			existingSession = createSession();
-		}
+        if (existingSession == null) {
+            existingSession = createSession();
+        }
 
-		if (existingSession != null) {
-			openCreateRepresentationWizard(existingSession);
-		}
+        if (existingSession != null) {
+            openCreateRepresentationWizard(existingSession);
+        }
 
-	}
+    }
 
-	private Session createSession() {
-		final CreateSessionResourceWizard wizard = new CreateSessionResourceWizard(
-				selection);
-		wizard.init(PlatformUI.getWorkbench(), selection);
-		final WizardDialog dialog = new WizardDialog(getShell(), wizard);
-		dialog.create();
-		dialog.getShell().setText("Create Representation File");
-		if (dialog.open() == Dialog.OK) {
+    private Session createSession() {
+        final CreateSessionResourceWizard wizard = new CreateSessionResourceWizard(selection);
+        wizard.init(PlatformUI.getWorkbench(), selection);
+        final WizardDialog dialog = new WizardDialog(getShell(), wizard);
+        dialog.create();
+        dialog.getShell().setText("Create Representation File");
+        if (dialog.open() == Dialog.OK) {
 
-			wizard.getCreatedSession()
-					.getTransactionalEditingDomain()
-					.getCommandStack()
-					.execute(
-							new RecordingCommand(wizard.getCreatedSession()
-									.getTransactionalEditingDomain()) {
+            wizard.getCreatedSession().getTransactionalEditingDomain().getCommandStack().execute(new RecordingCommand(wizard.getCreatedSession().getTransactionalEditingDomain()) {
 
-								@Override
-								protected void doExecute() {
-									wizard.getCreatedSession()
-											.addSemanticResource(
-													domainModelURI,
-													new NullProgressMonitor());
+                @Override
+                protected void doExecute() {
+                    wizard.getCreatedSession().addSemanticResource(domainModelURI, new NullProgressMonitor());
 
-								}
-							});
-			return wizard.getCreatedSession();
-		}
-		return null;
+                }
+            });
+            return wizard.getCreatedSession();
+        }
+        return null;
 
-	}
+    }
 
-	protected void openCreateRepresentationWizard(final Session existingSession) {
-		/*
-		 * Workaround to last minute NPE in
-		 * org.eclipse.sirius.ui.tools.internal.
-		 * actions.creation.CreateRepresentationAction$1
-		 * .run(CreateRepresentationAction.java:126)
-		 * 
-		 * We are forcing to create the uiSession so that the wizard does not
-		 * fail. To reproduce the problem you have to use this action on an
-		 * .ecore file from the Java perspective.
-		 */
-		IEditingSession uiSession = SessionUIManager.INSTANCE
-				.getOrCreateUISession(existingSession);
-		uiSession.open();
-		existingSession
-				.getTransactionalEditingDomain()
-				.getCommandStack()
-				.execute(
-						new RecordingCommand(existingSession
-								.getTransactionalEditingDomain()) {
+    protected void openCreateRepresentationWizard(final Session existingSession) {
+        /*
+         * Workaround to last minute NPE in
+         * org.eclipse.sirius.ui.tools.internal.
+         * actions.creation.CreateRepresentationAction$1
+         * .run(CreateRepresentationAction.java:126) We are forcing to create
+         * the uiSession so that the wizard does not fail. To reproduce the
+         * problem you have to use this action on an .ecore file from the Java
+         * perspective.
+         */
+        IEditingSession uiSession = SessionUIManager.INSTANCE.getOrCreateUISession(existingSession);
+        uiSession.open();
+        existingSession.getTransactionalEditingDomain().getCommandStack().execute(new RecordingCommand(existingSession.getTransactionalEditingDomain()) {
 
-							@Override
-							protected void doExecute() {
-								ViewpointSelectionCallback selection = new ViewpointSelectionCallback();
-								for (Viewpoint previouslySelected : existingSession
-										.getSelectedViewpoints(false)) {
-									selection.deselectViewpoint(
-											previouslySelected,
-											existingSession,
-											new NullProgressMonitor());
-								}
-								selection.selectViewpoint(EcoreToolsViewpoints
-										.fromViewpointRegistry().design(),
-										existingSession,
-										new NullProgressMonitor());
-								selection.selectViewpoint(EcoreToolsViewpoints
-										.fromViewpointRegistry().review(),
-										existingSession,
-										new NullProgressMonitor());
-								selection.selectViewpoint(EcoreToolsViewpoints
-										.fromViewpointRegistry().archetype(),
-										existingSession,
-										new NullProgressMonitor());
-								selection.selectViewpoint(EcoreToolsViewpoints
-										.fromViewpointRegistry().generation(),
-										existingSession,
-										new NullProgressMonitor());
-							}
-						});
+            @Override
+            protected void doExecute() {
+                Set<Viewpoint> fromEcoreTools = EcoreToolsViewpoints.fromViewpointRegistry().all();
+                ViewpointSelectionCallback selection = new ViewpointSelectionCallback();
+                for (Viewpoint previouslySelected : existingSession.getSelectedViewpoints(false)) {
+                    if (fromEcoreTools.contains(previouslySelected)) {
+                        selection.deselectViewpoint(previouslySelected, existingSession, new NullProgressMonitor());
+                    }
+                }
+                selection.selectViewpoint(EcoreToolsViewpoints.fromViewpointRegistry().design(), existingSession, new NullProgressMonitor());
+                selection.selectViewpoint(EcoreToolsViewpoints.fromViewpointRegistry().review(), existingSession, new NullProgressMonitor());
+                selection.selectViewpoint(EcoreToolsViewpoints.fromViewpointRegistry().archetype(), existingSession, new NullProgressMonitor());
+                selection.selectViewpoint(EcoreToolsViewpoints.fromViewpointRegistry().generation(), existingSession, new NullProgressMonitor());
+            }
+        });
 
-		CreateRepresentationWizard wizard = new CreateRepresentationWizard(
-				existingSession);
-		wizard.init();
-		final WizardDialog dialog = new WizardDialog(getShell(), wizard);
-		dialog.create();
-		dialog.getShell().setText("Create Representation Wizard");
-		dialog.open();
-	}
+        CreateRepresentationWizard wizard = new CreateRepresentationWizard(existingSession);
+        wizard.init();
+        final WizardDialog dialog = new WizardDialog(getShell(), wizard);
+        dialog.create();
+        dialog.getShell().setText("Create Representation Wizard");
+        dialog.open();
+    }
 }
--- org.eclipse.emf.ecoretools.design/src/org/eclipse/emf/ecoretools/design/EcoreToolsViewpoints.java
+++ org.eclipse.emf.ecoretools.design/src/org/eclipse/emf/ecoretools/design/EcoreToolsViewpoints.java
@@ -10,43 +10,69 @@
  *******************************************************************************/
 package org.eclipse.emf.ecoretools.design;
 
+import java.util.Set;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.sirius.business.api.componentization.ViewpointRegistry;
 import org.eclipse.sirius.viewpoint.description.Viewpoint;
 
+import com.google.common.collect.Sets;
+
 public class EcoreToolsViewpoints {
 
-	private ViewpointRegistry registry;
+    private ViewpointRegistry registry;
 
-	public EcoreToolsViewpoints(ViewpointRegistry registry) {
-		this.registry = registry;
-	}
+    public EcoreToolsViewpoints(ViewpointRegistry registry) {
+        this.registry = registry;
+    }
 
-	public Viewpoint design() {
-		return registry
-				.getViewpoint(URI
-						.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Design"));
-	}
+    public Viewpoint design() {
+        URI uri = URI.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Design");
+        return getViewpointOrNull(uri);
+    }
 
-	public Viewpoint archetype() {
-		return registry
-				.getViewpoint(URI
-						.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Archetype"));
-	}
+    protected Viewpoint getViewpointOrNull(URI uri) {
+        try {
+            return registry.getViewpoint(uri);
+        } catch (Exception e) {
+            /*
+             * If we can't retrieve the viewpoint then it means we don't
+             * contribute it.
+             */
+        }
+        return null;
+    }
 
-	public Viewpoint review() {
-		return registry
-				.getViewpoint(URI
-						.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Review"));
-	}
+    public Viewpoint archetype() {
+        return getViewpointOrNull(URI.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Archetype"));
+    }
 
-	public Viewpoint generation() {
-		return registry
-				.getViewpoint(URI
-						.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Generation"));
-	}
+    public Viewpoint review() {
+        return getViewpointOrNull(URI.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Review"));
+    }
 
-	public static EcoreToolsViewpoints fromViewpointRegistry() {
-		return new EcoreToolsViewpoints(ViewpointRegistry.getInstance());
-	}
+    public Viewpoint generation() {
+        return getViewpointOrNull(URI.createURI("viewpoint:/org.eclipse.emf.ecoretools.design/Generation"));
+    }
+
+    public Set<Viewpoint> all() {
+        Set<Viewpoint> ecoreToolsViewpoints = Sets.newLinkedHashSet();
+        Viewpoint vp = design();
+        if (vp != null)
+            ecoreToolsViewpoints.add(vp);
+        vp = archetype();
+        if (vp != null)
+            ecoreToolsViewpoints.add(vp);
+        vp = review();
+        if (vp != null)
+            ecoreToolsViewpoints.add(vp);
+        vp = generation();
+        if (vp != null)
+            ecoreToolsViewpoints.add(vp);
+        return ecoreToolsViewpoints;
+    }
+
+    public static EcoreToolsViewpoints fromViewpointRegistry() {
+        return new EcoreToolsViewpoints(ViewpointRegistry.getInstance());
+    }
 }
