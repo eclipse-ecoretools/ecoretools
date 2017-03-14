79d3c2fde5c712bf27c056cb11a029d317406f8c
cbrun <cedric.brun@obeo.fr>
2017-03-14 11:29:26 +0100

    Bump version to 3.3.0
    Change-Id: I1563da914edb84219f6d890abbbf5aa7c2c566c9

---
+++ org.eclipse.emf.ecoretools.build/bump-version.sh
@@ -0,0 +1,4 @@
+
+export VERSION=$1
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:0.22.0:set-version -DnewVersion=$VERSION-SNAPSHOT
+sed -i -e 's/export VERSION=.*$/export VERSION="$VERSION"/g' publish-nightly.sh
