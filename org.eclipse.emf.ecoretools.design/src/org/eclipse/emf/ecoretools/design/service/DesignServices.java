c961a8a79e7ba58eda0296e123a7b6e87914f722
Laurent Redor <laurent.redor@obeo.fr>
2019-01-24 12:27:24 +0100

    [538853] Adapt EcoreTools code to this Sirius issue
    The bugzilla 538853 clears diagram dependencies to Sirius custom code.
    This commit adapts the EcoreTools code to these changes.
    This commit will be valid as soon as EcoreTools will use a Sirius 6.2.0
    version.
    
    Bug: 538853
    Change-Id: I6da2d308d4caeeeed50e4d654d57f2b7e5205f5d
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

--- org.eclipse.emf.ecoretools.design/src/org/eclipse/emf/ecoretools/design/service/DesignServices.java
+++ org.eclipse.emf.ecoretools.design/src/org/eclipse/emf/ecoretools/design/service/DesignServices.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2016 THALES GLOBAL SERVICES and Others
+ * Copyright (c) 2013, 2019 THALES GLOBAL SERVICES and Others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -69,6 +69,8 @@ import org.eclipse.sirius.diagram.EdgeArrows;
 import org.eclipse.sirius.diagram.EdgeTarget;
 import org.eclipse.sirius.diagram.business.api.query.DDiagramQuery;
 import org.eclipse.sirius.diagram.business.internal.helper.task.operations.CreateViewTask;
+import org.eclipse.sirius.diagram.business.internal.metamodel.helper.ContainerMappingHelper;
+import org.eclipse.sirius.diagram.business.internal.metamodel.helper.ContentHelper;
 import org.eclipse.sirius.diagram.business.internal.query.DDiagramInternalQuery;
 import org.eclipse.sirius.diagram.description.AbstractNodeMapping;
 import org.eclipse.sirius.diagram.description.DiagramElementMapping;
@@ -880,30 +882,26 @@ public class DesignServices extends EReferenceServices {
 
 		if (containerView instanceof DSemanticDiagram) {
 
-			for (DiagramElementMapping mapping : (((DSemanticDiagram) containerView).getDescription()
-					.getAllContainerMappings())) {
+            for (DiagramElementMapping mapping : ContentHelper.getAllContainerMappings((((DSemanticDiagram) containerView).getDescription()), false)) {
 				String domainClass = ((AbstractNodeMapping) mapping).getDomainClass();
 				if (modelAccessor.eInstanceOf(semanticElement, domainClass) && !mapping.isCreateElements()) {
 					mappings.add(mapping);
 				}
 			}
-			for (DiagramElementMapping mapping : (((DSemanticDiagram) containerView).getDescription()
-					.getAllNodeMappings())) {
+            for (DiagramElementMapping mapping : ContentHelper.getAllNodeMappings((((DSemanticDiagram) containerView).getDescription()), false)) {
 				String domainClass = ((AbstractNodeMapping) mapping).getDomainClass();
 				if (modelAccessor.eInstanceOf(semanticElement, domainClass) && !mapping.isCreateElements()) {
 					mappings.add(mapping);
 				}
 			}
 		} else if (containerView instanceof DNodeContainer) {
-			for (DiagramElementMapping mapping : (((DNodeContainer) containerView).getActualMapping()
-					.getAllContainerMappings())) {
+            for (DiagramElementMapping mapping : ContainerMappingHelper.getAllContainerMappings((((DNodeContainer) containerView).getActualMapping()))) {
 				String domainClass = ((AbstractNodeMapping) mapping).getDomainClass();
 				if (modelAccessor.eInstanceOf(semanticElement, domainClass) && !mapping.isCreateElements()) {
 					mappings.add(mapping);
 				}
 			}
-			for (DiagramElementMapping mapping : (((DNodeContainer) containerView).getActualMapping()
-					.getAllNodeMappings())) {
+            for (DiagramElementMapping mapping : ContainerMappingHelper.getAllNodeMappings((((DNodeContainer) containerView).getActualMapping()))) {
 				String domainClass = ((AbstractNodeMapping) mapping).getDomainClass();
 				if (modelAccessor.eInstanceOf(semanticElement, domainClass) && !mapping.isCreateElements()) {
 					mappings.add(mapping);
