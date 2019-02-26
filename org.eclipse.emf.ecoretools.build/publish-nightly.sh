c503e63fcec66f694ceebeb0dc5007ed06bf37b1
Pierre Guilet <pierre.guilet@obeo.fr>
2019-02-26 17:18:17 +0100

    [version] Bump version to 3.3.1
    Change-Id: I931f048db522172479e158fabc20f82295615cbd
    Signed-off-by: Pierre Guilet <pierre.guilet@obeo.fr>
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- org.eclipse.emf.ecoretools.build/publish-nightly.sh
+++ org.eclipse.emf.ecoretools.build/publish-nightly.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ====================================================================
-# Copyright (c) 2013 Obeo
+# Copyright (c) 2013, 2019 Obeo
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
@@ -31,7 +31,7 @@ provided by the Hudson build.
 set -e
 
 # The full version (should be taken as an argument)
-export VERSION=3.3.0
+export VERSION=3.3.1
 
 # The type of build being published
 export BUILD_TYPE="nightly"
