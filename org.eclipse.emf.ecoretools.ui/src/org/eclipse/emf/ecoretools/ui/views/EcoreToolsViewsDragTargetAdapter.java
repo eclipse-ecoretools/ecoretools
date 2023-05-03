e64dec8125165e2303698170c0e33512ab27acf7
Pierre-Charles David <pierre-charles.david@obeo.fr>
2023-05-03 17:50:52 +0200

    [2] Remove usage of the deprecated LocalSelectionTransfer
    org.eclipse.ui.views.navigator.LocalSelectionTransfer has been
    deprecated for a while, and actually removed from the platform for
    4.28 (2023-06). Replace with
    org.eclipse.jface.util.LocalSelectionTransfer.
    
    Bug: https://github.com/eclipse-ecoretools/ecoretools/issues/2
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- org.eclipse.emf.ecoretools.ui/src/org/eclipse/emf/ecoretools/ui/views/EcoreToolsViewsDragTargetAdapter.java
+++ org.eclipse.emf.ecoretools.ui/src/org/eclipse/emf/ecoretools/ui/views/EcoreToolsViewsDragTargetAdapter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 THALES GLOBAL SERVICES.
+ * Copyright (c) 2009, 2023 THALES GLOBAL SERVICES and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,6 +16,7 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecoretools.ui.views.EReferencesContentProvider.WrappedEClass;
+import org.eclipse.jface.util.LocalSelectionTransfer;
 import org.eclipse.jface.util.TransferDragSourceListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
@@ -23,86 +24,61 @@ import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.dnd.DragSourceAdapter;
 import org.eclipse.swt.dnd.DragSourceEvent;
 import org.eclipse.swt.dnd.Transfer;
-import org.eclipse.ui.views.navigator.LocalSelectionTransfer;
 
 /**
  * A simple drag target adapter for {@link LocalSelectionTransfer}.
- * 
+ *
  * @author mchauvin
  */
-public class EcoreToolsViewsDragTargetAdapter extends DragSourceAdapter
-		implements TransferDragSourceListener {
+public class EcoreToolsViewsDragTargetAdapter extends DragSourceAdapter implements TransferDragSourceListener {
 
-	private ISelectionProvider provider;
+    private ISelectionProvider provider;
 
-	/**
-	 * Construct a new drag target adapter from the selection provider given as
-	 * parameter.
-	 * 
-	 * @param provider
-	 *            the selection provider
-	 */
-	public EcoreToolsViewsDragTargetAdapter(final ISelectionProvider provider) {
-		this.provider = provider;
-	}
+    /**
+     * Construct a new drag target adapter from the selection provider given as parameter.
+     *
+     * @param provider
+     *            the selection provider
+     */
+    public EcoreToolsViewsDragTargetAdapter(final ISelectionProvider provider) {
+        this.provider = provider;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.eclipse.jface.util.TransferDragSourceListener#getTransfer()
-	 */
-	public Transfer getTransfer() {
-		return LocalSelectionTransfer.getInstance();
-	}
+    @Override
+    public Transfer getTransfer() {
+        return LocalSelectionTransfer.getTransfer();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.eclipse.swt.dnd.DragSourceAdapter#dragStart(org.eclipse.swt.dnd.DragSourceEvent)
-	 */
-	@Override
-	public void dragStart(final DragSourceEvent event) {
-		final ISelection selection = provider.getSelection();
-		if (selection instanceof StructuredSelection) {
-			StructuredSelection sSelection = (StructuredSelection) selection;
-			List<EObject> ecoreObjects = new ArrayList<EObject>(
-					sSelection.size());
-			Iterator<Object> it = sSelection.iterator();
-			while (it.hasNext()) {
-				Object obj = it.next();
-				if (obj instanceof EObject) {
-					ecoreObjects.add((EObject) obj);
-				} else if (obj instanceof WrappedEClass) {
-					ecoreObjects.add(((WrappedEClass) obj).getWrappedEClass());
-				}
+    @Override
+    public void dragStart(final DragSourceEvent event) {
+        final ISelection selection = provider.getSelection();
+        if (selection instanceof StructuredSelection) {
+            StructuredSelection sSelection = (StructuredSelection) selection;
+            List<EObject> ecoreObjects = new ArrayList<>(sSelection.size());
+            Iterator<?> it = sSelection.iterator();
+            while (it.hasNext()) {
+                Object obj = it.next();
+                if (obj instanceof EObject) {
+                    ecoreObjects.add((EObject) obj);
+                } else if (obj instanceof WrappedEClass) {
+                    ecoreObjects.add(((WrappedEClass) obj).getWrappedEClass());
+                }
+            }
+            ISelection unwrapped = new StructuredSelection(ecoreObjects);
+            LocalSelectionTransfer.getTransfer().setSelection(unwrapped);
+            LocalSelectionTransfer.getTransfer().setSelectionSetTime(event.time & 0xFFFFFFFFL);
+            event.doit = true;
+        }
+    }
 
-			}
-			ISelection unwrapped = new StructuredSelection(ecoreObjects);
-			LocalSelectionTransfer.getInstance().setSelection(unwrapped);
-			LocalSelectionTransfer.getInstance().setSelectionSetTime(
-					event.time & 0xFFFFFFFFL);
-			event.doit = true;
-		}
-	}
+    @Override
+    public void dragSetData(final DragSourceEvent event) {
+        event.data = LocalSelectionTransfer.getTransfer().getSelection();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.eclipse.swt.dnd.DragSourceAdapter#dragSetData(org.eclipse.swt.dnd.DragSourceEvent)
-	 */
-	@Override
-	public void dragSetData(final DragSourceEvent event) {
-		event.data = LocalSelectionTransfer.getInstance().getSelection();
-	}
-
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.eclipse.swt.dnd.DragSourceAdapter#dragFinished(org.eclipse.swt.dnd.DragSourceEvent)
-	 */
-	@Override
-	public void dragFinished(final DragSourceEvent event) {
-		LocalSelectionTransfer.getInstance().setSelection(null);
-		LocalSelectionTransfer.getInstance().setSelectionSetTime(0);
-	}
+    @Override
+    public void dragFinished(final DragSourceEvent event) {
+        LocalSelectionTransfer.getTransfer().setSelection(null);
+        LocalSelectionTransfer.getTransfer().setSelectionSetTime(0);
+    }
 }
