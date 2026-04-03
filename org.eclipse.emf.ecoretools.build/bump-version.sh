c64e28a3565eae5026d6665df841cf240c951bfd
Pierre-Charles David <pierre-charles.david@obeo.fr>
2026-04-03 11:44:03 +0200

    [23] Update target platforms
    Bug: https://github.com/eclipse-ecoretools/ecoretools/issues/23
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- org.eclipse.emf.ecoretools.build/bump-version.sh
+++ org.eclipse.emf.ecoretools.build/bump-version.sh
@@ -1,4 +1,4 @@
 
 export VERSION=$1
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:0.22.0:set-version -DnewVersion=$VERSION-SNAPSHOT
-sed -i -e "s/export VERSION=.*$/export VERSION="$VERSION"/g" publish-nightly.sh
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:5.0.2:set-version -DnewVersion=$VERSION-SNAPSHOT
+sed -i -e "s/export VERSION=.*$/export VERSION="$VERSION"/g" publish-nightly-jiro.sh
